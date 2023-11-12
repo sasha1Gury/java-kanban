926989b2d763e8d47d1bf916c25d38b51b2113b1
sasha1Gury <sashagury456123@mail.ru>
2023-11-12 14:08:39 +0300

    feat: add default history

--- src/javakanban/managers/Managers.java
+++ src/javakanban/managers/Managers.java
@@ -4,6 +4,8 @@ import javakanban.managers.HistoryManager;
 import javakanban.managers.InMemoryHistoryManager;
 import javakanban.managers.InMemoryTaskManager;
 
+import java.io.File;
+
 public class Managers {
     public static TaskManager getDefault() {
         return new InMemoryTaskManager();
@@ -11,5 +13,9 @@ public class Managers {
 
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
+    }
+
+    public static FileBackedTasksManager getDefaultFileManager() {
+        return new FileBackedTasksManager(new File("resource/tasks.csv"));
     }
 }
