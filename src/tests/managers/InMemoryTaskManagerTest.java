ee66e441ac75e303cb27faa711f09ee5ccc91959
sasha1Gury <sashagury456123@mail.ru>
2023-12-06 01:54:55 +0300

    feat: add setUpper with init tasks

--- src/tests/managers/InMemoryTaskManagerTest.java
+++ src/tests/managers/InMemoryTaskManagerTest.java
@@ -1,13 +1,26 @@
 package managers;
 
 import javakanban.managers.InMemoryTaskManager;
+
+import javakanban.tasks.Epic;
+import javakanban.tasks.Subtask;
+import javakanban.tasks.Task;
 import org.junit.jupiter.api.BeforeEach;
 
+
 public class InMemoryTaskManagerTest extends TaskManagerTest<InMemoryTaskManager> {
+
     @BeforeEach
     void setUp() {
         taskManager = new InMemoryTaskManager();
     }
 
+    @BeforeEach
+    void setUpper() {
+        task = new Task("epic1", "de");
+        epic = new Epic("epic", "descr");
+        epic.setId(1);
+        subtask = new Subtask("epic1", "de", epic.getId());
+    }
 
 }
