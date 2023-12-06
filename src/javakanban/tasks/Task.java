ec2f67f0f7237e555f767d6821f0b83027bf14fa
sasha1Gury <sashagury456123@mail.ru>
2023-12-07 01:57:27 +0300

    feat: add startTime and Duration in Tasks

--- src/javakanban/tasks/Task.java
+++ src/javakanban/tasks/Task.java
@@ -1,10 +1,15 @@
 package javakanban.tasks;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class Task {
     protected String taskName;
     protected String description;
     public Status status;
     protected int id;
+    protected Duration duration;
+    protected LocalDateTime startTime;
     protected Type type;
 
     public Task() {
@@ -22,6 +27,15 @@ public class Task {
     public Task(String taskName, String description) {
         this.taskName = taskName;
         this.description = description;
+        this.status = Status.NEW;
+        this.type = Type.TASK;
+    }
+
+    public Task(String taskName, String description, int duration, LocalDateTime startTime) {
+        this.taskName = taskName;
+        this.description = description;
+        this.duration = Duration.ofMinutes(duration);
+        this.startTime = startTime;
         this.status = Status.NEW;
         this.type = Type.TASK;
     }
