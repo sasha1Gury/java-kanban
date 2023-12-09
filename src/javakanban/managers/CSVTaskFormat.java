943e9a84fc974f78394a623eec61948da4a5910c
sasha1Gury <sashagury456123@mail.ru>
2023-12-09 03:58:17 +0300

    feat: now CSVFormatter consider duration and localDateTime

--- src/javakanban/managers/CSVTaskFormat.java
+++ src/javakanban/managers/CSVTaskFormat.java
@@ -2,26 +2,31 @@ package javakanban.managers;
 
 import javakanban.tasks.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CSVTaskFormat {
     public static Task taskFromString(String value) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyy.MM.dd HH:mm");
         final String[] values = value.split(",");
         final int id = Integer.parseInt(values[0]);
         final Type type = Type.valueOf(values[1]);
         final String name = values[2];
         final Status status = Status.valueOf(values[3]);
         final String description = values[4];
+        final int duration = Integer.parseInt(values[5]);
+        final LocalDateTime localDateTime = LocalDateTime.parse(values[6], formatter);
 
         if (type == Type.TASK) {
-            return new Task(id, name, description, status);
+            return new Task(id, name, description, status, duration, localDateTime);
         }
         if (type == Type.SUBTASK) {
-            final int epicId= Integer.parseInt(values[5]);
-            return new Subtask(id, name, description, status, epicId);
+            final int epicId= Integer.parseInt(values[7]);
+            return new Subtask(id, name, description, status, epicId, duration, localDateTime);
         }
-        return new Epic(id, name, description, status);
+        return new Epic(id, name, description, status, duration, localDateTime);
     }
 
     public static List<Integer> historyFromString(String value) {
