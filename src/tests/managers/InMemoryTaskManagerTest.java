e7ec8bcd6cd00a62b99e0ced6860a144177ce41a
sasha1Gury <sashagury456123@mail.ru>
2023-12-09 14:13:36 +0300

    test: test prioritizedTasks

--- src/tests/managers/InMemoryTaskManagerTest.java
+++ src/tests/managers/InMemoryTaskManagerTest.java
@@ -5,7 +5,13 @@ import javakanban.managers.InMemoryTaskManager;
 import javakanban.tasks.Epic;
 import javakanban.tasks.Subtask;
 import javakanban.tasks.Task;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
 
 
 public class InMemoryTaskManagerTest extends TaskManagerTest<InMemoryTaskManager> {
@@ -21,5 +27,23 @@ public class InMemoryTaskManagerTest extends TaskManagerTest<InMemoryTaskManager
         epic = new Epic("epic", "descr");
         epic.setId(1);
         subtask = new Subtask("epic1", "de", epic.getId());
+    }
+
+    @Test
+    void shouldGetPrioritizedTasks() {
+        Task task1 = new Task("Task", "description", 60, LocalDateTime.of(2023, 1, 1, 14, 30));
+        Task task2 = new Task("Task", "description", 60, LocalDateTime.of(2023, 1, 1, 13, 30));
+        Task task3 = new Task("Task", "description", 60, LocalDateTime.of(2023, 1, 1, 15, 30));
+
+        taskManager.createTasks(task1);
+        taskManager.createTasks(task2);
+        taskManager.createTasks(task3);
+
+        List<Task> prioritizedTasks = taskManager.getPrioritizedTasks();
+
+        // Проверяем, что порядок сортировки соответствует ожиданиям
+        Assertions.assertEquals(task2, prioritizedTasks.get(0));
+        Assertions.assertEquals(task1, prioritizedTasks.get(1));
+        Assertions.assertEquals(task3, prioritizedTasks.get(2));
     }
 }
