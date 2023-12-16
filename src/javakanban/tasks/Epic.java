fb27f868798b22ae2fc5830611869b94193f7d50
sasha1Gury <sashagury456123@mail.ru>
2023-12-16 18:43:58 +0300

    feat: add new constructor with date and duration

--- src/javakanban/tasks/Epic.java
+++ src/javakanban/tasks/Epic.java
@@ -16,6 +16,12 @@ public class Epic extends Task {
         this.type = Type.EPIC;
     }
 
+    public Epic(String taskName, String description, int duration, LocalDateTime startTime){
+        super(taskName, description, duration, startTime);
+        this.status = Status.NEW;
+        this.type = Type.EPIC;
+    }
+
     public Epic(Integer id, String taskName, String description, Status status, int duration, LocalDateTime startTime) {
         super(taskName, description, duration, startTime);
         this.id = id;
