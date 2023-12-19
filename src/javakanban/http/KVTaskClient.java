dddc144f8739b13773781dc1c3a8f25bf4d66404
sasha1Gury <sashagury456123@mail.ru>
2023-12-19 19:45:37 +0300

    feat: add KVTaskClient

---
+++ src/javakanban/http/KVTaskClient.java
@@ -0,0 +1,64 @@
+package javakanban.http;
+
+import javakanban.server.KVServer;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class KVTaskClient {
+    private final String url;
+    private final String apiToken;
+
+    public KVTaskClient(int port) {
+        url = "http://localhost:" + port + "/";
+        apiToken = register(url);
+    }
+
+    private String register(String url) {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url + "register"))
+                    .GET()
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String load(String key) {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url + "load/" + key + "?API_TOKEN=" + apiToken))
+                    .GET()
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void put(String key, String value) {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url + "save/" + key + "?API_TOKEN=" + apiToken))
+                    .POST(HttpRequest.BodyPublishers.ofString(value))
+                    .build();
+            HttpResponse<Void> response = client.send(request, HttpResponse.BodyHandlers.discarding());
+
+            if (response.statusCode() != 200) {
+                throw new RuntimeException("Ошибка отправки данных на сервер: " + response.statusCode());
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
