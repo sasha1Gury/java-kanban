f4bac1c762fd851ab13e9ca8e50df0e577fc3826
sasha1Gury <sashagury456123@mail.ru>
2023-12-21 01:06:12 +0300

    main:

--- src/javakanban/Main.java
+++ src/javakanban/Main.java
@@ -1,6 +1,24 @@
 package javakanban;
 
+import com.google.gson.Gson;
+import javakanban.http.HttpTaskServer;
+import javakanban.server.KVServer;
+import javakanban.tasks.Task;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Gson gson = new Gson();
+        KVServer kvServer = new KVServer();
+        kvServer.start();
+        HttpTaskServer httpTaskServer = new HttpTaskServer();
+        httpTaskServer.start();
+
+        Task newTask = new Task("Task1", "des");
+        System.out.println(newTask.getId());
+
+        String json = gson.toJson(newTask);
+        System.out.println(json);
     }
 }
