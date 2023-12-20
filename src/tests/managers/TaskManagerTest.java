67903ec10a2642b0a9e8dc1ee9180780c285dac0
sasha1Gury <sashagury456123@mail.ru>
2023-12-20 23:47:24 +0300

    feat: fields now protected

--- src/tests/managers/TaskManagerTest.java
+++ src/tests/managers/TaskManagerTest.java
@@ -10,11 +10,11 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-abstract class TaskManagerTest<T extends TaskManager> {
-    T taskManager;
-    Task task;
-    Epic epic;
-    Subtask subtask;
+public abstract class TaskManagerTest<T extends TaskManager> {
+    protected T taskManager;
+    protected Task task;
+    protected Epic epic;
+    protected Subtask subtask;
 
     @Test
     public void taskShouldBeAddedInListAfterCreate() {
